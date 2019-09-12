cbc3da280fecd30c06f9f015f55fd5f60796f883
zhou <zhoushengzhe01@gmail.com>
2019-09-12 15:04:14 +0800

    源码
    源码

---
+++ dtmob/shell/moneyd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PATH=/usr/local/php56/bin:$PATH
+
+cd /home/wwwroot/diantui
+
+php artisan moneyd
---
+++ dtmob/shell/moneyw.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PATH=/usr/local/php56/bin:$PATH
+
+cd /home/wwwroot/diantui
+
+php artisan moneyw
